04e7ded95c6ae17c4f30eeab939aa041049caf8f
Surajmaster <glorioussuraj@gmail.com>
2022-08-22 16:13:20 +0530

    initial commit

---
+++ src/main/java/com/project/ELearningManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningManagementSystemApplication.class, args);
+	}
+
+}
